c7cec387c7ac9a9685250c423ec5e657530c7b30
Alyssa Clarke <alyssa.clarke00@gmail.com>
2019-12-28 21:53:14 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -20,7 +20,7 @@ def monopoly_with_third_tier
         :reading_railroad=>{},
         :pennsylvania_railroad=>{},
         :b_and_o_railroad=>{},
-        :key_four=>"value"
+        :shortline_railroad=>{}
       },
       :rent_in_dollars=>{
         :one_piece_owned=>25,
